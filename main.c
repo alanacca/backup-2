4a610e3ae2b1526ef54d5d6ca1464144e4252582
alanacca <44409612+alanacca@users.noreply.github.com>
2019-04-01 23:17:42 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,72 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <funcoes.h>
-
-/*faça um programa em c/c++ que realize as seguintes operações:
-
-a)Leia uma matriz Anxm de um arquivo e armazene em um vetor
-
-b)Leia uma matriz Bnxm de um arquivo e armazene em um vetor
-
-c)Imprima na tela o resultados das seguintes operações
-
-    1. AXB
-
-    2. AXBt
-
-    3. At
-
-    4. Triangular Superior de A
-
-    5. Linha K de A(ler k do teclado)
-
-    6. Diagonal de A
-
-    7. Coluna K de A(ler k do teclado)*/
-
-
-
+#include <funcao.h>
 
 int main()
 
 {
 
-    int i,j,k,l;
-
     int n,m,p,q;
 
     FILE *arqA;
+    FILE *arqB;
 
     arqA = fopen("matrizA.txt","r");
-
-    if(arqA == NULL){
-
-        printf("arquivo A zerado\n");
-
-    }else{
-
-        printf("pegado com sucesso A\n");
-
-    }
+    arqB = fopen("matrizB.txt","r");
 
     if(arqA != NULL){
         fscanf(arqA,"%d %d",&n,&m);
     }
 
-    FILE *arqB;
-
-    arqB = fopen("matrizB.txt","r");
-
-    if(arqB == NULL){
-
-        printf("arquivo B zerado\n");
-
-    }else{
-
-        printf("pegado com sucesso B\n");
-
-    }
-
     if(arqB != NULL){
         fscanf(arqB,"%d %d",&p,&q);
     }
@@ -79,9 +30,9 @@ int main()
 
     recMatrizA = LerArqMatriz(arqA,n,m);
 
-    for(i=0;i<n;i++){
+    for(int i=0;i<n;i++){
         printf("\n");
-        for(j=0;j<m;j++){
+        for(int j=0;j<m;j++){
             printf("%f ",recMatrizA[i*m+j]);
         }
     }
@@ -91,80 +42,61 @@ int main()
 
     recMatrizB = LerArqMatriz(arqB,p,q);
 
-    for(k=0;k<p;k++){
+    for(int k=0;k<p;k++){
         printf("\n");
-        for(l=0;l<q;l++){
+        for(int l=0;l<q;l++){
             printf("%f ",recMatrizB[k*q+l]);
         }
     }
-
-
-
-
-
-
-    FILE *arqB;
-
-    arqB = fopen("matrizB.txt","r");
-
-    if(arqB == NULL){
-
-        printf("arquivo B zerado\n");
-    }
-
-    if(arqB != NULL){
-        fscanf(arqB,"%d %d",&p,&q);
-    }
-
-    float *recMatrizA;
-
-    float *recMatrizB;
-
-    printf("\nmatriz A:\n");
-
-    recMatrizA = LerArqMatriz(arqA,n,m);
-
-    for(i=0;i<n;i++){
-        printf("\n");
-        for(j=0;j<m;j++){
-            printf("%f ",recMatrizA[i*m+j]);
-        }
-    }
-
-
-     printf("\nmatriz B:\n");
-
-    recMatrizB = LerArqMatriz(arqB,p,q);
-
-    for(k=0;k<p;k++){
-        printf("\n");
-        for(l=0;l<q;l++){
-            printf("%f ",recMatrizB[k*q+l]);
-        }
-    }
+    int opcao;
+    int k;
     do{
     /*1. AXB
-
     2. AXBt
-
     3. At
-
     4. Triangular Superior de A
-
     5. Linha K de A(ler k do teclado)
-
     6. Diagonal de A
-
     7. Coluna K de A(ler k do teclado)*/
-        int opção;
-        printf("*****MENU*****\n");
+        printf("\n*****MENU*****\n");
         printf("AXB[1]\n");
         printf("AXBt[2]\n");
-        printf("Triangular Superior de A[3]\n");
-        printf("Linha K de A[4]\n");
-        printf("Diagonal Principal de A\n");
-
-        scanf("%d",)
+        printf("Transposta de A[3]\n");
+        printf("Triangular Superior de A[4]\n");
+        printf("Linha K de A[5]\n");
+        printf("Diagonal Principal de A[6]\n");
+        printf("Coluna K de A[7]\n");
+        printf("Sair do programa[0]\n");
+        printf("*****MENU*****\n");
+        scanf("%d",&opcao);
+        switch(opcao){
+          case 1:
+            multMat(n,m,p,q,recMatrizA,recMatrizB);
+            printf("\n");
+            break;
+          case 2:
+            MultABT(n,m,p,q,recMatrizA,recMatrizB);
+            printf("\n");
+            break;
+          case 3:
+            transposta(n,m,recMatrizA);
+            printf("\n");
+            break;
+          case 4:
+            TriangularSuperior(n,recMatrizA);
+            printf("\n");
+            break;
+          case 5:
+            printf("Insira a linha que deseja:\n");
+            scanf("%d",&k);
+            RetornaLinha(n,m,recMatrizA,k-1);
+            printf("\n");
+            break;
+          case 6:
+            DiagPrin(n,m,recMatrizA);         
+            printf("\n");
+            break;
+        }
     }while(opcao != 0);
 
 
